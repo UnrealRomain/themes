d1c9b7230a6c720f5c4bddf46e008279af338c8a
UnrealRomain <romainpeyrot2@gmail.com>
2018-11-19 14:47:43 +0100

    fdb

--- themezero/functions.php
+++ themezero/functions.php
@@ -1,1 +1,6 @@
 <?php
+@import url("../twentyseventeen/style.css");
+function display_hello()
+{
+    echo 'Bonjour les o';
+}
--- themezero/index.php
+++ themezero/index.php
@@ -1,2 +1,2 @@
 <?php
-echo 'Bonjour je suis Romain';
+display_hello();
